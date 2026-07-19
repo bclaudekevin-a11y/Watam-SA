2d3e6d38e728a866e88e64d0719f2882b62d0049
Kevin Bougouma <bclaudekevin@gmail.vom>
2026-07-19 03:49:41 +0000

    Fix database configuration

--- Watam/settings.py
+++ Watam/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +77,9 @@ WSGI_APPLICATION = 'Watam.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600)}
 
 
 # Password validation
@@ -120,5 +120,3 @@ STATIC_URL = 'static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
