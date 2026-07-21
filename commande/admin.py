b40555264bee66c4ffee77c6ef3761434b4f295d
Kevin Bougouma <bclaudekevin@gmail.vom>
2026-07-21 00:15:28 +0000

    categories et date commande

--- commande/admin.py
+++ commande/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from.models import Commande
 admin.site.register(Commande)
+class CommandeAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'produit', 'telephone', 'ville', 'quartier', 'date_commande')
+    list_filter = ('date_commande', 'ville')
+    search_fields = ('nom', 'telephone', 'produit__nom')
+    ordering = ('-date_commande',)
+    date_hierarchy = 'date_commande'
